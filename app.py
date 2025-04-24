1ea390f14950cfaedc1b8f4511f30840ca5ed235
truongng1 <truong.m.nguyen@sjsu.edu>
2025-04-23 20:23:37 -0700

    add navigation bar

--- app.py
+++ app.py
@@ -3,17 +3,43 @@ from dash import Dash, html, dcc
 
 app = Dash(__name__, use_pages=True)
 server = app.server
+
 app.layout = html.Div([
     html.Nav([
+        html.Div("Group 5", style={
+            "color": "white",
+            "fontWeight": "bold",
+            "fontSize": "20px",
+            "marginLeft": "20px"
+        }),
         html.Ul([
-            html.Li(dcc.Link(f"{page['name']}", href=page["relative_path"]))
+            html.Li(
+                dcc.Link(
+                    page["name"],
+                    href=page["relative_path"],
+                    style={"color": "white", "textDecoration": "none", "padding": "0 15px"}
+                )
+            )
             for page in dash.page_registry.values()
-        ])
-    ]),
-    dash.page_container
-])
+        ],
+            style={
+                "display": "flex",
+                "justifyContent": "center",
+                "alignItems": "center",
+                "listStyleType": "none",
+                "margin": "0",
+                "padding": "0",
+                "flexGrow": "1"
+            })
+    ], style={
+        "display": "flex",
+        "alignItems": "center",
+        "backgroundColor": "#111",
+        "padding": "10px"
+    }),
 
-server = app.server  # Needed for GAE
+    html.Div(dash.page_container, style={"padding": "20px"})
+])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
