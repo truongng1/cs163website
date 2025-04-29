f55141faa6ed3fec6212721414711d40c5437c48
Samuel <litaksum59@gmail.com>
2025-04-29 00:44:30 -0700

    adding plots to page 4

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import dash
 from dash import Dash, html, dcc
+import flask
 
-app = Dash(__name__, use_pages=True)
-server = app.server
+server = flask.Flask(__name__)
+
+app = Dash(__name__, server = server,use_pages=True)
+
+@server.route('/static/<path:path>')
+def serve_static(path):
+    return flask.send_from_directory('static', path)
 
 app.layout = html.Div([
     html.Nav([
@@ -43,4 +49,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run(debug=True)
--- pages/page4.py
+++ pages/page4.py
@@ -4,5 +4,63 @@ from dash import html
 dash.register_page(__name__, name="Major Findings")
 
 layout = html.Div([
-    html.H2("Major Findings")
+    html.H2("Major Findings", style={"textAlign": "center", "marginBottom": "20px", "fontFamily": "Courier New"}),
+    html.H2("Seasonal Patterns in 911 Call Volume, particularly the impact of Weather and Holidays", style={"fontFamily": "Courier New"}),
+    html.Div([
+        html.Div([
+            html.Img(src='../static/result_1_1.png',
+                     style={
+                         'maxWidth': '85%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+            html.H3('Clear Seasonal fluctuations, with recurring peaks and dips that align with the weather patterns.\n',
+                    style={'textAlign': 'center', "fontFamily": "Courier New"})
+        ], style={'textAlign': 'center', 'margin': '10px'}),
+
+        html.Div([
+            html.Img(src='../static/result_1_2.png',
+                     style={
+                         'maxWidth': '85%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+            html.H3([
+                'Call volumes rise in mid-year but decline during the holiday season (November and December)', html.Br(),
+                'Contrary to common expectations of increased holiday-related incidents.', html.Br(),
+                'Warmer weather may lead to more outdoor activities, resulting in higher 911 call volumes.'
+            ], style={'textAlign': 'center', "fontFamily": "Courier New"})
+        ], style={'textAlign': 'center', 'margin': '10px'})
+
+    ], style={
+        'display': 'flex',  # Enables side-by-side layout
+        'justifyContent': 'center',  # Centers the images horizontally
+        'alignItems': 'flex-start',  # Aligns the images vertically
+        'marginTop': '20px'
+    }),
+
+
+    html.Div([
+        html.Div([
+            html.Img(src='../static/result_1_3.png',
+                     style={
+                         'maxWidth': '85%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+        ], style={'textAlign': 'center', 'margin': '10px'}),
+        html.Div([
+            html.Img(src='../static/result_1_4.png',
+                     style={
+                         'maxWidth': '85%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+        ], style={'textAlign': 'center', 'margin': '10px'}),
+    ], style={
+        'display': 'flex',  # Enables side-by-side layout
+        'justifyContent': 'center',  # Centers the images horizontally
+        'alignItems': 'flex-start',  # Aligns the images vertically
+        'marginTop': '20px'
+    }),
 ])
