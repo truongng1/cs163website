828d6ccde42fc9e8218466a86bf737d74f30332a
Samuel <litaksum59@gmail.com>
2025-04-29 17:44:56 -0700

    update page 4

--- pages/page4.py
+++ pages/page4.py
@@ -6,87 +6,40 @@ dash.register_page(__name__, name="Major Findings")
 layout = html.Div([
     html.H1("Major Findings", style={"textAlign": "center", "marginBottom": "20px", "fontFamily": "Nunito"}),
     html.H2("Seasonal Patterns in 911 Call Volume, particularly the impact of Weather and Holidays", style={'textAlign': 'center',"fontFamily": "Nunito"}),
-    html.Div([
-        html.Div([
-            html.Img(src='../static/result_1_1.png',
-                     style={
-                         'maxWidth': '100%',  # Adjust width to fit side by side
-                         'height': 'auto',
-                         'margin': '10px'
-                     }),
-            html.Li('Clear Seasonal fluctuations, with recurring peaks and dips that align with the weather patterns.\n',
-                    style={"fontFamily": "Nunito"})
-        ], style={'margin': '10px'}),
-
-        html.Div([
-            html.Img(src='../static/result_1_2.png',
-                     style={
-                         'maxWidth': '100%',  # Adjust width to fit side by side
-                         'height': 'auto',
-                         'margin': '10px'
-                     }),
-            html.Li('Call volumes rise in mid-year but decline during the holiday season (November and December)',
-                    style = {'fontFamily': 'Nunito'}),
-            html.Br(),
-
-            html.Li('Contrary to common expectations of increased holiday-related incidents.',
-                    style = {'fontFamily': 'Nunito'}),
-            html.Br(),
-
-            html.Li('Warmer weather may lead to more outdoor activities, resulting in higher 911 call volumes.',
-                    style = {'fontFamily': 'Nunito'}),
-
-        ], style={'margin': '10px'})
-
-    ], style={
-        'display': 'flex',  # Enables side-by-side layout
-        'justifyContent': 'center',  # Centers the images horizontally
-        'alignItems': 'flex-start',  # Aligns the images vertically
-        'marginTop': '20px'
-    }),
-
-    html.Div([
-        html.Div([
-            html.Img(src='../static/result_1_5.png',
-                     style={
-                         'maxWidth': '100%',  # Adjust width to fit side by side
-                         'height': 'auto',
-                         'margin': '10px'
-                     }),
-        ], style={'textAlign': 'center', 'margin': '10px'}),
-        html.Div([
-            html.Img(src='../static/result_1_5.png',
-                     style={
-                         'maxWidth': '100%',  # Adjust width to fit side by side
-                         'height': 'auto',
-                         'margin': '10px'
-                     }),
-        ], style={'textAlign': 'center', 'margin': '10px'}),
-    ], style={
-        'display': 'flex',  # Enables side-by-side layout
-        'justifyContent': 'center',  # Centers the images horizontally
-        'alignItems': 'flex-start',  # Aligns the images vertically
-        'marginTop': '20px'
-    }),
-
-
     # html.Div([
     #     html.Div([
-    #         html.Img(src='../static/result_1_3.png',
+    #         html.Img(src='../static/result_1_1.png',
     #                  style={
     #                      'maxWidth': '100%',  # Adjust width to fit side by side
     #                      'height': 'auto',
     #                      'margin': '10px'
     #                  }),
-    #     ], style={'textAlign': 'center', 'margin': '10px'}),
+    #         html.Li('Clear Seasonal fluctuations, with recurring peaks and dips that align with the weather patterns.\n',
+    #                 style={"fontFamily": "Nunito"})
+    #     ], style={'margin': '10px'}),
+
+
+
     #     html.Div([
-    #         html.Img(src='../static/result_1_4.png',
+    #         html.Img(src='../static/result_1_2.png',
     #                  style={
     #                      'maxWidth': '100%',  # Adjust width to fit side by side
     #                      'height': 'auto',
     #                      'margin': '10px'
     #                  }),
-    #     ], style={'textAlign': 'center', 'margin': '10px'}),
+    #         html.Li('Call volumes rise in mid-year but decline during the holiday season (November and December)',
+    #                 style = {'fontFamily': 'Nunito'}),
+    #         html.Br(),
+
+    #         html.Li('Contrary to common expectations of increased holiday-related incidents.',
+    #                 style = {'fontFamily': 'Nunito'}),
+    #         html.Br(),
+
+    #         html.Li('Warmer weather may lead to more outdoor activities, resulting in higher 911 call volumes.',
+    #                 style = {'fontFamily': 'Nunito'}),
+
+    #     ], style={'margin': '10px'})
+
     # ], style={
     #     'display': 'flex',  # Enables side-by-side layout
     #     'justifyContent': 'center',  # Centers the images horizontally
@@ -94,5 +47,55 @@ layout = html.Div([
     #     'marginTop': '20px'
     # }),
 
+    html.Img(src='../static/result_1_1.png',
+            style={
+                'maxWidth': '100%',  # Adjust width to fit side by side
+                'height': 'auto',
+                'margin': '10px'
+            }),
 
+    html.Li('Clear Seasonal fluctuations, with recurring peaks and dips that align with the weather patterns.\n',
+        style={"fontFamily": "Nunito"}),
+
+
+    html.Img(src='../static/result_1_2.png',
+                style={
+                    'maxWidth': '100%',  # Adjust width to fit side by side
+                    'height': 'auto',
+                    'margin': '10px'
+                }),
+    html.Li('Call volumes rise in mid-year but decline during the holiday season (November and December)',
+            style = {'fontFamily': 'Nunito'}),
+    html.Br(),
+
+    html.Li('Contrary to common expectations of increased holiday-related incidents.',
+            style = {'fontFamily': 'Nunito'}),
+    html.Br(),
+
+    html.Li('Warmer weather may lead to more outdoor activities, resulting in higher 911 call volumes.',
+            style = {'fontFamily': 'Nunito'}),
+
+    html.Div([
+        html.Div([
+            html.Img(src='../static/result_1_5.png',
+                     style={
+                         'maxWidth': '100%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+        ], style={'textAlign': 'center', 'margin': '10px'}),
+        html.Div([
+            html.Img(src='../static/result_1_5.png',
+                     style={
+                         'maxWidth': '100%',  # Adjust width to fit side by side
+                         'height': 'auto',
+                         'margin': '10px'
+                     }),
+        ], style={'textAlign': 'center', 'margin': '10px'}),
+    ], style={
+        'display': 'flex',  # Enables side-by-side layout
+        'justifyContent': 'center',  # Centers the images horizontally
+        'alignItems': 'flex-start',  # Aligns the images vertically
+        'marginTop': '20px'
+    }),
 ])
